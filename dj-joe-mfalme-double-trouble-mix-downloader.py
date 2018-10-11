b244d742ea1184b6b2ba566e0fc06766e79d4e15
ikuamike <mikeikuam@gmail.com>
2018-10-11 14:37:04 +0200

    Added interactive option to choose mix

--- dj-joe-mfalme-double-trouble-mix-downloader.py
+++ dj-joe-mfalme-double-trouble-mix-downloader.py
@@ -1,6 +1,14 @@
+##############################
+# Author: Michael Ikua
+# Github: ikuamike
+# ############################
+
+
+
 # ========== Imports ==========
 
 from bs4 import BeautifulSoup
+import os
 import pprint
 import requests
 import sys
@@ -8,12 +16,16 @@ import sys
 # ========== Main variables ==========
 
 site_url = "http://www.deejayjoemfalme.com"
-mix_page = "http://www.deejayjoemfalme.com/index.php/mixes"
+mixes_page_url = "http://www.deejayjoemfalme.com/index.php/mixes"
 
 # ========== Functions ==========
 
 
 def download_mix(url, filename):
+    default_download_location = os.getenv('HOME') + '/Music'
+    print('\n[*] Downloading ' + filename + ' to ' + default_download_location + '\n')
+    filename = default_download_location + '/' + filename
+
     with open(filename, 'wb') as f:
         response = requests.get(url, stream=True)
         total_length = response.headers.get('content-length')
@@ -23,8 +35,9 @@ def download_mix(url, filename):
             dl += len(data)
             f.write(data)
             done = int(50 * dl / total_length)
-            sys.stdout.write('\r[{}{}]'.format('█' * done, '.' * (50-done)))
+            sys.stdout.write('\t\r[{}{}]'.format('█' * done, '.' * (50-done)))
             sys.stdout.flush()
+    print('\n\n[*] Done!')
 
 
 def extract_urls(html, search_phrase):
@@ -42,29 +55,37 @@ def extract_urls(html, search_phrase):
     return sorted(list(set(all_urls))) # sort to get latest as top
 
 def collect_urls():
-    mixes = {}
-    mix_page_urls = extract_urls(requests.get(mix_page).text, 'mixes-')
+    mixes = []
+    mix_page_urls = extract_urls(requests.get(mixes_page_url).text, 'mixes-')
     for url in mix_page_urls:
-        year = url[-4:]
         url = site_url + url
-        mixes[year] = extract_urls(requests.get(url).text, 'Double')
+        for link in extract_urls(requests.get(url).text, 'Double'):
+            mixes.append(link)
+
+        # year = url[-4:]
+        # url = site_url + url
+        # mixes[year] = extract_urls(requests.get(url).text, 'Double')
     return mixes
 
 def list_urls():
-    for key, value in collect_urls().items():
-        for index, url in enumerate(value):
-            filename = url.split('/')[-1].replace('%20', " ")
-            print("{}.  {}.".format(str(index), filename[:-4]))
+    urls_list = []
+    for index, url in enumerate(collect_urls()):
+        filename = url.split('/')[-1].replace('%20', " ")
+        urls_list.append(filename)
+        print("{}.  {}".format(str(index + 1), filename[:-4]))
+    return urls_list
 
 
 # ========== Main ==========
 
 def main():
-    list_urls()
+    filenames = list_urls()
+    download_choice = int(input("\nWhich mix would you like to Download?\n> ")) - 1
+    mixes = collect_urls()
+    download_mix(mixes[download_choice], filenames[download_choice])
     # pprint.pprint(collect_urls())
 
 
 main()
-# print('[*] Downloading ' + filename)
 
 
